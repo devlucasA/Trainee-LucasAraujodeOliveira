f164f5f866bb799aec33ccc82af844d35a8f3e7d
Lucas Araújo de Oliveira <129886429+devlucasA@users.noreply.github.com>
2024-03-28 14:40:47 -0300

    Add files via upload

---
+++ Módulos/B/part_1/script.js
@@ -0,0 +1,10 @@
+/*
+var nome = window.prompt('Qual é seu nome?')
+window.alert('é um grande parzer em te conhecer, ' + nome + '!')
+*/
+
+var n1 = Number(window.prompt('Digite um número: '))
+var n2 = Number(window.prompt('Digite outro número: '))
+var s = n1 + n2
+/*window.alert('A soma dos valores é ' + String(s))*/
+window.alert(`A soma entre ${n1} e ${n2} é igual a a ${s}`)
