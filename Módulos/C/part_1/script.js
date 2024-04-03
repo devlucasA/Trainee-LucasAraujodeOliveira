2a6044ba0a955cd24a707516a82e32814f97de50
Lucas Araújo de Oliveira <129886429+devlucasA@users.noreply.github.com>
2024-04-03 10:20:37 -0300

    Add files via upload

---
+++ Módulos/C/part_1/script.js
@@ -0,0 +1,12 @@
+
+var corpo = window.document.body
+
+var p1 = window.document.getElementsBytagName('p')[0]
+//window.document.write('Esta escrito assim assim: ' + p1.innerText)
+//p1.style.color = 'blue'
+//document.write(p1.innerText)
+
+var d = window.document.getElementById('msg')
+
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'
